123428ef6247d41124c1fff92fefd06812b75666
RomanRas <kannidze@gmail.com>
2023-11-06 21:36:11 +1000

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+//        Задача 1
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println();
+
+//        Задача 2
+        System.out.println("Задача 2");
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + upperFullName);
+        System.out.println();
+
+//        Задача 3
+        System.out.println("Задача 3");
+        fullName = "Иванов Семён Семёнович";
+        String fixedFullName = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф.И.О. сотрудника - " + fixedFullName);
     }
 }
